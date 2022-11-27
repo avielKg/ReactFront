a47ebd1dbc117f0093309adf523a40598181b0c6
אביאל קוגן <avichi@h-MacBook-Air-sl-byl.local>
2022-11-27 19:16:53 +0200

    color2

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Welcome = () => {
   const myStyle={
     backgroundImage: 
-    "url('https://djangoback.onrender.com/media/Posted_images/backgroundphoto.webp)",
-color:"black"
+    "url('https://djangoback.onrender.com/media/Posted_Images/backgroundphoto.webp)",
+color:"white"
 };
   return (
     <div style={myStyle}>
@@ -16,10 +16,10 @@ color:"black"
       <h4 align="center">this project was created using Django as a backend<br></br> and the front was created by React&Redux.js<br></br>
       enjoy your visit! </h4><br></br><br></br>
       <h3 align="center" style={{margin:"150px"}}>Find me on:<br></br>
-      <a href="https://www.linkedin.com/in/aviel-kogan-65a205237/"><img src="https://djangoback.onrender.com/media/Posted_images/linkedin.png" height="50px"></img></a>{"    "}
-      <a href="https://github.com/avielKg"><img src="https://djangoback.onrender.com/media/Posted_images/github-logo.png" height="50px"></img></a>{"    "}
-      <a href="mailto:aviel33223@gmail.com"><img src="https://djangoback.onrender.com/media/Posted_images/gmail.png" height="50px"></img></a>{"    "}
-      <a href="https://www.facebook.com/aviel33223/"><img src="https://djangoback.onrender.com/media/Posted_images/facebook.png" height="50px"></img></a>{"    "} 
+      <a href="https://www.linkedin.com/in/aviel-kogan-65a205237/"><img src="https://djangoback.onrender.com/media/Posted_Images/linkedin.png" height="50px"></img></a>{"    "}
+      <a href="https://github.com/avielKg"><img src="https://djangoback.onrender.com/media/Posted_Images/github-logo.png" height="50px"></img></a>{"    "}
+      <a href="mailto:aviel33223@gmail.com"><img src="https://djangoback.onrender.com/media/Posted_Images/gmail.png" height="50px"></img></a>{"    "}
+      <a href="https://www.facebook.com/aviel33223/"><img src="https://djangoback.onrender.com/media/Posted_Images/facebook.png" height="50px"></img></a>{"    "} 
       </h3>
       <h5 align="center">
         created by <a href="">Aviel kogan</a></h5>
